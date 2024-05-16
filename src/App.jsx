723c5083abb9067071c2f3d9668d2274b8926ada
Craig Clayton <craig__clayton@outlook.com>
2024-05-16 20:33:19 +0100

    boom

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import BillInput from './components/Bill-Input';
+import './App.css';
+import MeSatisfied from './components/MeSatisfied';
+import FriendSatisfied from './components/FriendSatisfied';
+
+function App() {
+  return (
+    <div>
+      <BillInput />
+      <MeSatisfied />
+      <FriendSatisfied />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Bill-Input.jsx
@@ -0,0 +1,12 @@
+function BillInput() {
+  return (
+    <div>
+      <label htmlFor='bill'>How much was the bill?</label>
+      <input
+        type='text'
+        id='bill'
+      />
+    </div>
+  );
+}
+export default BillInput;
